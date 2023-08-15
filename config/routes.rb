2b16ecce7dee2aef6bf75318762aaa2bf93ec522
David Blezinger <david.w.blezinger@gmail.com>
2023-08-15 16:48:05 +0200

    committed 3 - Wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'articles', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  post 'articles', to: 'articles#create'
+  get 'articles/:id/edit', to: 'articles#edit', as: 'article'
+  patch 'articles/:id/edit', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy'
 end
